ff5c78b1e75292108a04c92da0e1bf8083ab555d
Akash Kumar <akash.kumar@peakmind.in>
2022-04-21 10:54:26 +0530

    theme setup done

--- src/chakra/theme.js
+++ src/chakra/theme.js
@@ -1,9 +1,32 @@
 import { extendTheme } from "@chakra-ui/react"
 
+// Import Fonts
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
+
 export const theme = extendTheme({
   colors: {
     brand: {
       100: "#FF3c00",
     },
+  },
+
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+
+  // Apply global css
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+
+  // Override Componentes to match custom design
+  components: {
+    // Button
   },
 })
